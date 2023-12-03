6de496471d6789e970bb36518a40581344c7ccdd
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-12-03 17:49:48 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,10 +44,10 @@ export default function Home() {
                 </div> */}
 
                     {/* Input And Button  */}
-                    <div className="input bg-gray-950 px-5 py-6 rounded-xl border border-gray-700 shadow-md ">
+                    <div className="input bg-gray-950 px-5 py-6 rounded-xl border border-gray-600 shadow-md ">
 
                         <div className="flex justify-center mb-8">
-                            <img className='w-20' src="https://cdn-icons-png.flaticon.com/128/5535/5535652.png" alt="" />
+                            <img className='w-20 border rounded-full border-gray-600' src="https://cdn-icons-png.flaticon.com/128/11104/11104179.png" alt="img" />
                         </div>
 
                         {/* Input Tag */}
@@ -56,7 +56,7 @@ export default function Home() {
                             value={value}
                             onChange={(e) => setValue(e.target.value)}
                             className='bg-gray-950  rounded-lg border
-                            border-gray-700 py-2 px-2 outline-none w-80 lg:w-96 text-lg text-white mb-5'
+                            border-gray-600 py-2 px-2 outline-none w-80 lg:w-96 text-lg text-white mb-5'
                             placeholder='Enter Room Code'
                         />
                         <br />
@@ -68,6 +68,8 @@ export default function Home() {
                         >
                             Join Room
                         </button>
+
+                        {/* <h1 className='text-gray-700 text-center'>Powered By Devknus</h1> */}
                     </div>
 
                 </div>
